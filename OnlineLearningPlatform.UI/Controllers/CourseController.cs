68ac58f17151b644f137f50d4f3272d4255c75f5
Amir <amirmawla27@gmail.com>
2024-10-15 02:44:08 +0300

    Add courses in track and course details and other relation for controller and view

---
+++ OnlineLearningPlatform.UI/Controllers/CourseController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineLearningPlatform.BLL.Interfaces;
+using OnlineLearningPlatform.DAL.Entities;
+using OnlineLearningPlatform.UI.ViewModels;
+
+namespace OnlineLearningPlatform.UI.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CourseController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+     
+        public async Task<IActionResult> Index()
+        {
+            var Courses = await _unitOfWork.Courses.GetAllAsync();
+            return View(Courses);
+        }
+        public async Task<IActionResult> CourseData(int? id)
+        {
+            CourseWithContentsViewModel courseWithContents = new CourseWithContentsViewModel();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _unitOfWork.Courses.GetFirstOrDefaultWithIncludeAsync(c=>c.Id==id,c=>c.Contents);
+            var track = await _unitOfWork.Tracks.FindByExpress(t=>t.Id== course.TrackId);
+            
+            if (course == null)
+            {
+                return NotFound();
+            }
+            courseWithContents.Id = course.Id;
+            courseWithContents.Name = course.Title;
+            courseWithContents.Description = course.Description;
+            courseWithContents.TrackName = track.Name;
+            courseWithContents.contents = course.Contents.ToList();
+            courseWithContents.CreatedDate = course.CreationDate;
+            courseWithContents.TrackId = track.Id;
+
+            return View(courseWithContents);
+        }
+    }
+}
---
+++ OnlineLearningPlatform.UI/ViewModels/CourseWithContentsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using OnlineLearningPlatform.DAL.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineLearningPlatform.UI.ViewModels
+{
+    public class CourseWithContentsViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Name { get; set; }
+
+
+        [Required]
+        [MaxLength(150)]
+        public string Description { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        [Required]
+        public int TrackId { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string TrackName { get; set; }
+        public List<Content> contents { get; set; }
+    }
+}
