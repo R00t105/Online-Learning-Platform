3f26379dfcc0e699421c7c23f9e87646d4b94826
R00t105 <moclaymo@gmail.com>
2024-10-16 21:03:39 +0300

    Application User Cruds

--- OnlineLearningPlatform.UI/Controllers/ApplicationUserController.cs
+++ OnlineLearningPlatform.UI/Controllers/ApplicationUserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using OnlineLearningPlatform.BLL.Interfaces;
 using OnlineLearningPlatform.DAL.Entities;
 using OnlineLearningPlatform.UI.ViewModels;
@@ -78,16 +79,139 @@ namespace OnlineLearningPlatform.UI.Controllers
 
         #region Edit
 
+
+        [HttpGet]
         public async Task<IActionResult> Edit(int id)
         {
-            var user = _userManager.FindByIdAsync(id.ToString());
+            var user = await _userManager.FindByIdAsync(id.ToString());
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var userRoles = await _userManager.GetRolesAsync(user);
+            var userRoleName = userRoles.FirstOrDefault();
+
+            var role = await _roleManager.Roles.FirstOrDefaultAsync(r => r.Name == userRoleName);
+
+            var UserModel = new ApplicationUserViewModel()
+            {
+                Id = user.Id,
+                Name = user.UserName,
+                Email = user.Email,
+                BirthDate = user.BirthDate,
+                RoleId = role != null ? role.Id : 0
+
+            };
             var roles = await _roleManager.Roles.ToListAsync();
             ViewBag.Roles = roles.Select(r => new { r.Id, r.Name }).ToList();
-            return View("Edit", new ApplicationUserViewModel());
-        } 
+            return View("Edit", UserModel);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ApplicationUserViewModel UserEdits)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.UserName = UserEdits.Name;
+            user.Email = UserEdits.Email;
+            user.BirthDate = UserEdits.BirthDate;
+
+            var updateResult = await _userManager.UpdateAsync(user);
+
+            if (!updateResult.Succeeded)
+            {
+                foreach (var error in updateResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+                var roles = await _roleManager.Roles.ToListAsync();
+                ViewBag.Roles = roles.Select(r => new { r.Id, r.Name }).ToList();
+                return View("Edit", UserEdits);
+            }
+
+            var currentRoles = await _userManager.GetRolesAsync(user);
+            var currentRoleName = currentRoles.FirstOrDefault();
+
+            var newRole = await _roleManager.Roles.FirstOrDefaultAsync(r => r.Id == UserEdits.RoleId);
+
+            if (newRole != null && newRole.Name != currentRoleName)
+            {
+                if (currentRoleName != null)
+                {
+                    await _userManager.RemoveFromRoleAsync(user, currentRoleName);
+                }
+
+                await _userManager.AddToRoleAsync(user, newRole.Name);
+            }
+
+            return RedirectToAction("Users", "Dashboard");
+        }
 
         #endregion
 
+        #region Delete
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+            var UserModel = new ApplicationUserViewModel()
+            {
+                Id = user.Id,
+                Name = user.UserName,
+                Email = user.Email,
+                RegistrationDate = user.RegistrationDate,
+                BirthDate = user.BirthDate
+            };
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+            var userRoleName = userRoles.FirstOrDefault();
+
+            var role = await _roleManager.Roles.FirstOrDefaultAsync(r => r.Name == userRoleName);
+
+            UserModel.RoleId = role != null ? role.Id : 0;
+
+            ViewBag.RoleName = role != null ? role.Name : "No Role";
+
+
+            return View("Delete", UserModel);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _userManager.DeleteAsync(user);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Users", "Dashboard");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View("Delete", model: new ApplicationUserViewModel { Id = id });
+        } 
+
+        #endregion
 
     }
 }
